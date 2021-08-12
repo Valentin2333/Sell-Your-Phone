f5abcfa089faacadef0d87dd744f100c3ef1c27c
Valentin Tenev <v.tenev10@gmail.com>
2021-08-12 14:51:17 +0300

    updated memory field in Phone Model

--- sell_your_phone/phones/models.py
+++ sell_your_phone/phones/models.py
@@ -14,6 +14,13 @@ class Phone(models.Model):
     BRAND_CHOICE_GOOGLE = 'Google'
     BRAND_CHOICE_NOKIA = 'Nokia'
 
+    MEMORY_CHOICE_16 = 16
+    MEMORY_CHOICE_32 = 32
+    MEMORY_CHOICE_64 = 64
+    MEMORY_CHOICE_128 = 128
+    MEMORY_CHOICE_256 = 256
+    MEMORY_CHOICE_512 = 512
+
     BRAND_CHOICES = (
         (BRAND_CHOICE_APPLE, 'Apple'),
         (BRAND_CHOICE_SAMSUNG, 'Samsung'),
@@ -24,11 +31,19 @@ class Phone(models.Model):
         (BRAND_CHOICE_NOKIA, 'Nokia'),
     )
 
+    MEMORY_CHOICES = (
+        (MEMORY_CHOICE_16, 16),
+        (MEMORY_CHOICE_32, 32),
+        (MEMORY_CHOICE_64, 64),
+        (MEMORY_CHOICE_128, 128),
+        (MEMORY_CHOICE_256, 256),
+        (MEMORY_CHOICE_512, 512),
+    )
     brand = models.CharField(max_length=8, choices=BRAND_CHOICES)
     phone_model = models.CharField(max_length=12)
     year = models.PositiveIntegerField()
     description = models.TextField()
-    memory = models.PositiveIntegerField()
+    memory = models.PositiveIntegerField(choices=MEMORY_CHOICES)
     price = models.PositiveIntegerField()
     phone_number_regex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
     contact_number = models.CharField(validators=[phone_number_regex], max_length=16)
